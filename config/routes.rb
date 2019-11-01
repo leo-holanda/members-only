48d5ad8786bd4f9d142539d120638016fa189c3f
leo-holanda <leo.uldholanda@gmail.com>
2019-11-01 14:21:42 -0300

    Add bootstrap and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: "users#new"
   get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
