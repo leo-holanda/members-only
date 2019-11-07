96b66f833c214bf224f1d42b59ede5d39bfc4e61
leo-holanda <leo.uldholanda@gmail.com>
2019-11-07 16:12:56 -0300

    fix session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user.authenticate(params[:session][:password])
-
+            log_in user
+            redirect_to user
         else
             flash[:danger] = 'Invalid email/password combination.'
             render 'new'
         end
+    end
+    
+    def destroy
+        log_off
+        redirect_to root_path
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+  
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find_by(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "User created"
-      redirect_to root_url
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
