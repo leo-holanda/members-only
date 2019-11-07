e39d05a7cef4cb35f799a8fb5f92842971efe06e
leo-holanda <leo.uldholanda@gmail.com>
2019-11-07 17:42:32 -0300

    add initial remember_me

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,28 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true,
                          length: { :within => 6..40 }
+
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+                      
+		def User.new_token
+			SecureRandom.urlsafe_base64
+		end
+
+    def remember
+      self.remember_digest = User.new_token
+			update_attribute(:remember_digest, User.digest(remember_digest))
+		end              
+		
+		def authenticated?(remember_token)
+			return false if remember_digest.nil?
+			BCrypt::Password.new(remember_digest).is_password?(remember_token)
+		end
+
+		def forget
+			update_attribute(:remember_digest, nil)
+		end
 end
