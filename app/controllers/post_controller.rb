b0a25fcd1f5b4b74e92b7d4b8f54b160f3df83a8
leo-holanda <leo.uldholanda@gmail.com>
2020-01-21 14:19:51 -0300

    add require login in post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,23 @@
 class PostController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+  def index
+  
+  end
+
+  def new
+    
+  end
+  
+  def create
+
+  end
+
+  private 
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_path # halts request cycle
+    end
+  end
 end
