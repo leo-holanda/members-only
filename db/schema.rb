aa8b8d770aefadcaca867a105dce2deb23efa507
leo-holanda <leo.uldholanda@gmail.com>
2020-01-21 15:37:12 -0300

    add title in post table and text area in new post view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_174549) do
+ActiveRecord::Schema.define(version: 2020_01_21_182932) do
 
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
