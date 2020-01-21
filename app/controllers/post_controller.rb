ff5973f233eebcc0eabbf9dd397742b9465c7dfa
leo-holanda <leo.uldholanda@gmail.com>
2020-01-21 15:59:47 -0300

    add create post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,15 +2,22 @@ class PostController < ApplicationController
   before_action :require_login, only: [:new, :create]
 
   def index
-  
+    @user = User.find_by(params[:id])
   end
 
   def new
+    @user = User.find_by(params[:id])
     @post = Post.new
   end
   
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post created"
+      redirect_to user_post_index_path
+    else
+      render 'new'
+    end
   end
 
   private 
@@ -20,5 +27,9 @@ class PostController < ApplicationController
       flash[:error] = "You must be logged in to access this section"
       redirect_to root_path # halts request cycle
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
