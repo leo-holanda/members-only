f48b4ae3dd1cc12ed11e7b8e7b8fee98349cc238
leo-holanda <leo.uldholanda@gmail.com>
2020-01-23 16:01:57 -0300

    (supposedly) final fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
-  validates :title, presence: true, length: { maximum: 10}
-  validates :text, presence: true, length: { maximum: 50}
+  validates :title, presence: true, length: { maximum: 28}
+  validates :text, presence: true, length: { maximum: 280}
 
   belongs_to :user
   paginates_per 10
